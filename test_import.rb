37ef7205f580343f7b7b5218d4a15e3d959a3e17
UK <uk@Macmini-UK.local>
2015-09-19 19:52:57 +0900

    Project started.

--- test_import.rb
+++ test_import.rb
@@ -4,6 +4,7 @@ include SDL2
 
 if __FILE__ == $0
   SDL2.load_lib('libSDL2.dylib') # '/usr/local/lib/libSDL2.dylib'
+  SDL_SetMainReady()
   success = SDL_Init(SDL_INIT_EVERYTHING)
   puts "SDL_Init : #{success == 0 ? 'Success' : 'Failed'}"
   exit if success != 0
